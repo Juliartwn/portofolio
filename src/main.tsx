7273b26f21f0f8464ab3977e07299af687cda5c2
Juliartwn <juliartawan630@gmail.com>
2025-11-03 13:35:39 +0800

    feat: install SpeedInsights and analytics vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Portfolio from './components/pages/Portofolio';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Portfolio from "./components/pages/Portofolio";
+import { inject } from "@vercel/analytics";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Portfolio />
-  </StrictMode>,
-)
+    <SpeedInsights />
+  </StrictMode>
+);
+
+inject();
